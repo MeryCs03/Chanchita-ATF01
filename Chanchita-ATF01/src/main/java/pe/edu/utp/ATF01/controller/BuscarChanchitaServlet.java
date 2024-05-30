f05319850cf2cce66d1c149a5e68b87e1a33c8c9
MeryCs03 <145505838+MeryCs03@users.noreply.github.com>
2024-05-30 15:29:55 -0500

    Update BuscarChanchitaServlet.java

--- Chanchita-ATF01/src/main/java/pe/edu/utp/ATF01/controller/BuscarChanchitaServlet.java
+++ Chanchita-ATF01/src/main/java/pe/edu/utp/ATF01/controller/BuscarChanchitaServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.SQLException;
 
 @WebServlet(name = "BuscarChanchita", urlPatterns = "/BuscarChanchita")
@@ -21,6 +22,7 @@ public class BuscarChanchitaServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         this.doPost(req, resp);
+
     }
 
     @Override
@@ -32,28 +34,35 @@ public class BuscarChanchitaServlet extends HttpServlet {
             ATF1service service = new ATF1service(dao, cnx);
 
             BuscarDepositos res = service.BuscarChanchita(Long.parseLong(nroCuenta));
+            
+            if (res == null || res.depositos().isEmpty()) {
+                // No se encontraron resultados, redirigir a la página de búsqueda
+                resp.sendRedirect(req.getContextPath() + "/BuscarChanchita.jsp");
+            } else {
+                // Se encontraron resultados, continuar con la generación de la tabla y otros elementos
+                StringBuilder sb = new StringBuilder();
+                service.generarTablaDepositos(res.depositos(), sb);
+                service.generarTotalIngresos(res.depositos(), sb);
 
-            StringBuilder sb = new StringBuilder();
-            service.generarTablaDepositos(res.depositos(), sb);
-            service.generarTotalIngresos(res.depositos(), sb);
+                req.setAttribute("nombre_chanchita", res.nombre_chanchita());
+                req.setAttribute("nroCuenta", nroCuenta);
+                req.setAttribute("table", sb.toString());
 
-            req.setAttribute("nombre_chanchita", res.nombre_chanchita());
-            req.setAttribute("nroCuenta", nroCuenta);
-            req.setAttribute("table", sb.toString());
+                // Incluye el encabezado de la tabla en la variable tablaEncabezado
+                String tablaEncabezado = "<thead>" +
+                        "<tr>" +
+                        "<th>Nombre</th>" +
+                        "<th>Fecha</th>" +
+                        "<th>Detalle</th>" +
+                        "<th>Monto S/</th>" +
+                        "</tr>" +
+                        "</thead>";
 
-            // Incluye el encabezado de la tabla en la variable tablaEncabezado
-            String tablaEncabezado = "<thead>" +
-                    "<tr>" +
-                    "<th>Nombre</th>" +
-                    "<th>Fecha</th>" +
-                    "<th>Detalle</th>" +
-                    "<th>Monto S/</th>" +
-                    "</tr>" +
-                    "</thead>";
+                req.setAttribute("tablaEncabezado", tablaEncabezado);
 
-            req.setAttribute("tablaEncabezado", tablaEncabezado);
+                req.getRequestDispatcher("/BuscarChanchita.jsp").forward(req, resp);
+            }
 
-            req.getRequestDispatcher("/BuscarChanchita.jsp").forward(req, resp);
         } catch (SQLException | NamingException e) {
             throw new RuntimeException(e);
         }
