0a22eab809025a4be41fe0ce7bee79e6363d0c0d
MeryCs03 <145505838+MeryCs03@users.noreply.github.com>
2024-05-30 21:56:48 -0500

    Delete Chanchita-ATF01 directory

--- Chanchita-ATF01/src/main/java/pe/edu/utp/ATF01/model/response/BuscarDepositos.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.utp.ATF01.model.response;
-
-import pe.edu.utp.ATF01.model.Deposito;
-
-import java.util.List;
-
-public record BuscarDepositos(String nombre_chanchita, List<Deposito> depositos) {
-   
-}
